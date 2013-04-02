1d2542da0838f0d5ad4b8ffcdefccf29824ca13b
Enzo De Sena <enzodesena@me.com>
2013-04-02 21:34:34 +0200

    Very small change (testing gitweb).

--- src/test/elementaryop_test.cpp
+++ src/test/elementaryop_test.cpp
@@ -43,7 +43,7 @@ bool ElementaryOpTest() {
   
   
   
-  assert(IsEqual(Rem((Real) 2.0, 2.0), 0.0));
+  assert(IsEqual(Rem((Real)2.0, 2.0), 0.0));
   assert(IsEqual(Rem((Real)5.0, 2.0), 1.0));
   assert(IsEqual(Rem((Real)5.5, 2.0), 1.5));
   assert(IsEqual(Rem((Real)2.0, 3.0), 2.0));
