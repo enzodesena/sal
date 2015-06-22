918eeae487f1d9edb8e6d77e1e6d0080e7bdafab
Enzo De Sena <enzodesena@me.com>
2015-06-22 07:13:02 +0200

    Throw_line didn't work. Not sure why. Replace with a simple throw.

--- include/mcltypes.h
+++ include/mcltypes.h
@@ -13,9 +13,8 @@
   #define PI 3.141592653589793238462643383279502884197169399375105820974944
 #endif
 
-#ifndef throw_line
-  #define throw_line(arg) throw std::runtime_error(std::string("Error at line n. ") +std::string(std::to_string(__LINE__)) + std::string(" of file ") + std::string(__FILE__));
-#endif
+#define throw_line(arg) throw
+// #define throw_line(arg) throw std::runtime_error(std::string("Error at line n. ") +std::string(std::to_string(__LINE__)) + std::string(" of file ") + std::string(__FILE__));
 
 #include <complex>
 #include <vector>
