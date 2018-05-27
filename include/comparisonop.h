aab6908c02a889a8af693fbbdd19fb859dd3d951
Enzo <enzodesena@gmail.com>
2018-05-28 00:00:01 +0100

    Adjusted tabs

--- include/comparisonop.h
+++ include/comparisonop.h
@@ -23,22 +23,20 @@ namespace mcl {
 
 bool IsEqual(Real num_a, Real num_b, Real precision = VERY_SMALL);
 
-bool IsEqual(Complex num_a, Complex num_b,
-                     Real precision = VERY_SMALL);
+bool IsEqual(Complex num_a, Complex num_b, Real precision = VERY_SMALL);
 
 bool IsSmallerOrEqual(const Real num_a, const Real num_b,
-                              const Real precision = VERY_SMALL);
+                      const Real precision = VERY_SMALL);
 
 bool IsLargerOrEqual(const Real num_a, const Real num_b,
-                             const Real precision = VERY_SMALL);
+                     const Real precision = VERY_SMALL);
 
 bool AreAllSmallerOrEqual(const std::vector<Real>& vector_a,
                           const std::vector<Real>& vector_b);
 
 template<class T>
-bool IsEqual(const std::vector<T>& vector_a,
-                     const std::vector<T>& vector_b,
-                     Real precision = VERY_SMALL) noexcept {
+bool IsEqual(const std::vector<T>& vector_a, const std::vector<T>& vector_b,
+             Real precision = VERY_SMALL) noexcept {
   if ((Int)vector_a.size() != (Int)vector_b.size())
     return false;
   
@@ -50,30 +48,24 @@ bool IsEqual(const std::vector<T>& vector_a,
 }
 
 
-bool IsEqual(const std::vector<Int>& vector_a,
-                     const std::vector<Int>& vector_b);
+bool IsEqual(const std::vector<Int>& vector_a, const std::vector<Int>& vector_b);
 
-bool IsEqual(const Quaternion& quaternion_a,
-                     const Quaternion& quaternion_b);
+bool IsEqual(const Quaternion& quaternion_a, const Quaternion& quaternion_b);
 
 
 
 bool IsEqual(const Point& point_a, const Point& point_b,
-                     const Real precision = VERY_SMALL);
+             const Real precision = VERY_SMALL);
 
 bool IsEqual(std::vector<Point> points_a, std::vector<Point> points_b);
 
-bool IsEqual(const Real* input_data_a,
-             const Real* input_data_b,
-             const Int num_samples,
+bool IsEqual(const Real* input_data_a, const Real* input_data_b,
+             const Int num_samples, Real precision = VERY_SMALL);
+
+bool IsEqual(const Real* input_data_a, const std::vector<Real> input_data_b,
              Real precision = VERY_SMALL);
 
-bool IsEqual(const Real* input_data_a,
-             const std::vector<Real> input_data_b,
-             Real precision = VERY_SMALL);
-
-bool IsEqual(const std::vector<Real> input_data_b,
-             const Real* input_data_a,
+bool IsEqual(const std::vector<Real> input_data_b, const Real* input_data_a,
              Real precision = VERY_SMALL);
   
 /** Returns true if num is nan */
